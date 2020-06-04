7f007d9985cffc61e5e2c6b4c78390d7a5e57a11
karoldavid <k.zyskowski@live.com>
2020-06-04 09:40:33 +0200

    feat: refine error handling for non existing image or wrong image format

--- src/server.ts
+++ src/server.ts
@@ -13,44 +13,48 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  interface RequestWithQuery extends Request {
+    query: {
+      image_url: string;
+    };
+  }
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  app.get("/filteredImage", async (req: Request, res: Response) => {
+  app.get("/filteredImage", async (req: RequestWithQuery, res: Response) => {
+    const IMAGE_REG_EXP = /[\/.](jpg|jpeg)$/i;
     const { image_url } = req.query;
 
     if (!image_url) {
       return res.status(422).send("Missing query param image_url!");
     }
 
+    const isJpeg = IMAGE_REG_EXP.test(image_url);
+
+    if (!isJpeg) {
+      return res
+        .status(422)
+        .send("The requested resoure image_url must be of type jpg or jpeg!");
+    }
+
     const exists = await urlExist(image_url);
 
     if (!exists) {
-      return res.status(404).send("The image_url does not exist!");
+      return res
+        .status(404)
+        .send("The image_url does not exist or is not publicly accessible!");
     }
 
     const filtered_image_url = await filterImageFromURL(image_url);
 
-    res.status(200).sendFile(filtered_image_url, (err) => {
-      deleteLocalFiles([filtered_image_url]);
+    res.status(200).sendFile(filtered_image_url, async (err) => {
       if (err) {
         console.log(err);
         res.status(500).send(err.message);
+      }
+
+      try {
+        await deleteLocalFiles([filtered_image_url]);
+      } catch (err) {
+        console.log(err);
       }
     });
   });
